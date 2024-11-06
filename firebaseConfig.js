71ef82a2d42fc9e55cc83a4c3a528ec7937daeab
prajwald23 <141308061+prajwald23@users.noreply.github.com>
2024-11-06 09:57:44 +0530

    Initial commit

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgVWzml44MtvqdPae2RNiSxWLQURq5X-8",
+  authDomain: "activity-7-371d6.firebaseapp.com",
+  databaseURL: "https://activity-7-371d6-default-rtdb.firebaseio.com",
+  projectId: "activity-7-371d6", 
+  storageBucket: "activity-7-371d6.firebasestorage.app",
+  messagingSenderId: "1065127898409",
+  appId: "1:1065127898409:web:3de1c5fb9a7a431df91b38",
+  measurementId: "G-DXJTE0N0WB"
+};
+
+const App = initializeApp(firebaseConfig);
+export default App;
